e7ba6298705faeb915cacc2b674204f402596706
Mar Prieto N <mar.pnieto@gmail.com>
2022-05-22 20:37:11 +0200

    starting deliverables final version

--- javascript/01-deliverables/src/exercise-02.ts
+++ javascript/01-deliverables/src/exercise-02.ts
@@ -1,1 +1,14 @@
 console.log("************** DELIVERABLE 02 *********************");
+const concat = (...args: any[]) => {
+    const result = []
+    args.forEach(arg => result.push(...arg));
+    return result;
+};
+
+console.log(concat(["Hola"], ["que tal?"]));
+const object1 = {name: "cosa", properties: {isAlive: true, color: "green"}};
+const object2 = {name: "Batman", properties: {isAlive: true, color: "dark"}};
+const concatenated = concat([object1], [object2]);
+concatenated[0].name = "Superman"
+console.log(object1);
+console.log(concatenated)
--- javascript/01-deliverables/src/exercise-03.ts
+++ javascript/01-deliverables/src/exercise-03.ts
@@ -1,1 +1,13 @@
 console.log("************** DELIVERABLE 03 *********************");
+function clone(source) {
+  // Implementation here.
+}
+
+function merge(sourceA, sourceB) {
+      // Implementation here.
+}
+
+const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
+const b = { name: "Luisa", age: 31, married: true };
+
+merge(a, b); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
