73d55f202bb90aaa17bc51860526b12da674e8ef
Mar Prieto N <mar.pnieto@gmail.com>
2022-08-27 21:20:52 +0200

    review and doc js deliverables exercise 5 slot machine

--- javascript/01-deliverables/src/exercise-05.ts
+++ javascript/01-deliverables/src/exercise-05.ts
@@ -1,44 +1,90 @@
 console.log("************** DELIVERABLE 05 *********************");
+console.log(
+  "%cSLOT MACHINE -> play slot machine with booleans",
+  "color: blue; font-weight: bold"
+);
+
 enum WinProbability {
-  medium = 0.5,
-  intermediate = 0.7,
-  high = 0.9
+  normal = 0.5,
+  medium = 0.7,
+  high = 0.9,
 }
 
+type GambleModel = [boolean, boolean, boolean];
 
 class SlothMachine {
-    public coins: number;
-    private probability: number;
+  // Coins is the reward for winning
+  public coins: number;
+  // Change this property at constructor for set desired win probability
+  private probability: number;
 
-    constructor(){
-      this.coins = 0;
-      this.probability = WinProbability.intermediate;
-    }d
+  constructor() {
+    this.coins = 0;
+    this.probability = WinProbability.medium;
+  }
 
-    public play(): void{
-      this.coins++;
-      const gamble:boolean[] = [this.generateBoolean(), this.generateBoolean(), this.generateBoolean()];
-      if(gamble.some(move => move === false)){
-        console.log('Good luck next time!!');
-      } else {
-        console.log(`Congratulations!!!. You won ${this.coins} coins!!`);
-        this.resetCoins();
-      } 
-    }
-
-    private resetCoins():void{
-      this.coins = 0;
-    }
-
-    private generateBoolean():boolean{
-      return Math.random() < this.probability
+  /**
+   * Generate a gamble with three ramdon booleans
+   */
+  public play(): void {
+    this.addCoin();
+    const gamble: GambleModel = [
+      this.generateBoolean(),
+      this.generateBoolean(),
+      this.generateBoolean(),
+    ];
+    console.log("You played one coin and you're gamble is: ", gamble);
+    if (this.isWinningGamble(gamble)) {
+      console.log(
+        `Good luck next time!! Jackpot is ${
+          this.coins === 1 ? "1 coin" : `${this.coins} coins`
+        }`
+      );
+    } else {
+      console.log(`Congratulations!!!. You won ${this.coins} coins!!`);
+      this.resetCoins();
     }
   }
-  
-  const machine1 = new SlothMachine();
-  console.log(machine1.coins);
-  machine1.play(); // "Good luck next time!!"
-  machine1.play(); // "Good luck next time!!"
-  machine1.play(); // "Congratulations!!!. You won 3 coins!!"
-  machine1.play(); // "Good luck next time!!"
-  machine1.play(); // "Congratulations!!!. You won 2 coins!!"
+
+  /**
+   * Add one coins to general counter when play a gamble
+   *
+   */
+  private addCoin(): void {
+    this.coins++;
+  }
+
+  /**
+   * Set counter to 0
+   */
+  private resetCoins(): void {
+    this.coins = 0;
+  }
+
+  /**
+   * Generates ramdom booleans. Obtain true as result depends on probability class prop.
+   * More closer to 1, is more likely to obtain false as outcome
+   * @returns {boolean} ramdom boolean
+   */
+  private generateBoolean(): boolean {
+    return Math.random() < this.probability;
+  }
+
+  /**
+   * Determine if all booleans of a gamble are true
+   * @param gamble represents player's gamble
+   * @returns true if it's a winning gamble
+   */
+  private isWinningGamble(gamble: GambleModel): boolean {
+    return gamble.some((move) => move === false);
+  }
+}
+
+const machine1 = new SlothMachine();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
