60bd674abbcc45dea3849e9cbd0f5f4ae1405ec5
Mar Prieto N <mar.pnieto@gmail.com>
2022-08-27 19:14:15 +0200

    deliverables js practice 1 clean test cases

--- javascript/01-deliverables/src/exercise-01.ts
+++ javascript/01-deliverables/src/exercise-01.ts
@@ -1,102 +1,31 @@
 console.log("************** DELIVERABLE 01 *********************");
 
-const test = ["Ana", "Pablo", "Laura"];
-const test2 = [
-  {
-    id: 1,
-    name: "Rex",
-    type: "dog",
-    tricks: [
-      {
-        id: 1,
-        name: "sit",
-        learned: true,
-        expertise: 3,
-      },
-      {
-        id: 2,
-        name: "hand",
-        learned: false,
-        expertise: null,
-      },
-    ],
-    owner: {
-      name: "Pepa",
-      adress: "C/ Valencia 4"
-    }
-  },
-  {
-    id: 2,
-    name: "Laika",
-    type: "dog",
-    tricks: [
-      {
-        id: 1,
-        name: "sit",
-        learned: false,
-        expertise: null,
-      },
-      {
-        id: 2,
-        name: "hand",
-        learned: false,
-        expertise: null,
-      },
-    ],
-    owner: {
-      name: "Rigoberta",
-      adress: "C/ Turín 2"
-    }
-  },
-];
+type ArrDeep<T> = T[];
 
+const test: ArrDeep<string> = ["Ana", "Pablo", "Laura"];
+console.log("EXAMPLE:", test);
 // head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento.
 
-const head = ([firstItem, ...others]: any[]): any => firstItem;
+const head = ([firstItem, ...others]: any[]) => firstItem;
 
-
+console.log("HEAD", head(test));
 
 // tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento.
-const tail = ([firstItem, ...args]: any[]): any => {
-    return args
-}; 
-
+const tail = ([firstItem, ...others]: any[]): any => {
+  return others;
+};
+console.log("TAIL: ", tail(test));
 
 // init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último.
-const init = (array) => array.reduce((acc, item, index) => {
-    if(index < array.length - 1){
-      acc.push(item)
-    }
-    return acc;
-  }, []);
+const init = (array: Array<any>) => {
+  return array.slice(0, array.length - 1);
+};
+
+console.log("INIT: ", init(test));
 
 // last (inmutable), tal que, dado un array como entrada devuelva el último elemento.
 const last = (array: any[]): any[] => {
-    return array[array.length - 1]
-}; 
+  return array[array.length - 1];
+};
 
-/**
- * Test
- * TODO - Object are not inmutables
- *      - types
- */
-// console.log(head(test));
-const objectItem = head(test2);
-// objectItem[0].type = "cambiado";
-console.log(objectItem);
-objectItem.id = 40;
-console.log(test2)
-
-const testTail = tail(test);
-const testTail1 = tail(test2);
-// testTail1[0].name = "cambiado";
-console.log(testTail1)
-
-// Init
-
-const testInit = tail(test);
-testInit[0] = "Lorenza";
-console.log("INIT", test)
-const testInit1 = tail(test2);
-testInit1[0].name = "cambiado";
-console.log(test2)
+console.log("LAST: ", last(test));
