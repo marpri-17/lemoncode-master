3339f9e0865c926806faafc99e5dbc193f4b606b
Mar Prieto N <mar.pnieto@gmail.com>
2022-11-24 19:24:20 +0100

    solved bundling assets problem

--- bundling/advanced/src/services/quoteService.ts
+++ bundling/advanced/src/services/quoteService.ts
@@ -1,11 +1,12 @@
 import { ServerQuote } from "../models/quote.models";
+import quotesMock from "./mocks/quotes.json";
 
 export function getQuote(): Promise<ServerQuote> {
   /*   return fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=1").then(
     (resp) => resp.json()
   ); */
 
-  return fetch("./mocks/quotesMock.json")
+  return fetch(quotesMock)
     .then((resp) => {
       console.log(resp);
       return resp.json();
--- bundling/advanced/src/titleComponent.tsx
+++ bundling/advanced/src/titleComponent.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import * as image from "./content/logo_2.png";
+import image from "./content/logo_2.png";
 
 export function TitleComponent() {
   return (
     <div className="container">
-      <img src="./content/logo_2.png" alt="Logo Lemoncode" />
+      <img src={image} alt="Logo Lemoncode" />
       <h1 className="my-title">Hola mundo</h1>
     </div>
   );
